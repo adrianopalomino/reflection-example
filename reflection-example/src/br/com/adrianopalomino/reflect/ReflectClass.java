9d84f707080e392380dcd84bdbe61cf22e2a493b
adrianopalomino <adrianoplmn66@gmail.com>
2019-05-16 00:01:41 -0300

    retirada method get

--- reflection-example/src/br/com/adrianopalomino/reflect/ReflectClass.java
+++ reflection-example/src/br/com/adrianopalomino/reflect/ReflectClass.java
@@ -8,10 +8,6 @@ public class ReflectClass {
 
 	private int attributeOne;
 
-	public int getAttributeOne() {
-		return attributeOne;
-	}
-
 	// this private method will be invoked by another class
 	private void setAttributeOne(int attributeOne) {
 		this.attributeOne = attributeOne;
